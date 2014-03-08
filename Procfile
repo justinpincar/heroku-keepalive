ec9bff4189034739e932bbb5fe6628eb9071b27e
Justin Pincar <justinpincar@gmail.com>
2014-03-07 17:30:53 -0800

    Initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: node worker.js
+
---
+++ worker.js
@@ -0,0 +1,24 @@
+var cronJob = require('cron').CronJob;
+var request = require('request');
+var _ = require('underscore');
+
+var urls = [
+  "http://snips.mew.io",
+  "http://tigerli.ly"
+];
+
+var ping = function() {
+  _.each(urls, function(url) {
+    console.log("Pinging url: " + url);
+    request(url, function (error, response, body) {
+      // Ignore response.
+    });
+  });
+};
+
+new cronJob('0 */2 * * * *', function() {
+  ping();
+}, null, true, null);
+
+ping()
+
